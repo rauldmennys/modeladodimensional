728ca1fecd3c61d81da50d9786710b8788d0a577
Raul Davila Menis <davilamennys@gmail.com>
2025-09-08 10:53:18 -0600

    version inicial repo modelado dimensional scripts

---
+++ Esquema INT/IncrementalLoads.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [int].[IncrementalLoads] (
+    [LoadDateKey] INT            IDENTITY (1, 1) NOT NULL,
+    [TableName]   NVARCHAR (100) NOT NULL,
+    [LoadDate]    DATETIME       NOT NULL,
+    CONSTRAINT [PK_LoadDates] PRIMARY KEY CLUSTERED ([LoadDateKey] ASC)
+);
+
+
+
---
+++ Esquema INT/Lineage.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [int].[Lineage] (
+    [LineageKey]     INT            IDENTITY (1, 1) NOT NULL,
+    [TableName]      NVARCHAR (200) NOT NULL,
+    [StartLoad]      DATETIME       NOT NULL,
+    [FinishLoad]     DATETIME       NULL,
+    [LastLoadedDate] DATETIME       NOT NULL,
+    [Status]         NVARCHAR (1)   CONSTRAINT [DF_Lineage_Status] DEFAULT (N'P') NOT NULL,
+    [Type]           NVARCHAR (1)   CONSTRAINT [DF_Lineage_Type] DEFAULT (N'F') NOT NULL,
+    CONSTRAINT [PK_Integration_Lineage] PRIMARY KEY CLUSTERED ([LineageKey] ASC)
+);
+
+
+
+
+
+
+
+
+
